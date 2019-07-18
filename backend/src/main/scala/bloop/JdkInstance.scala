69624b71a8eeecc50ce04b3228ec685dc401d7c1
Piotr Kołaczkowski <pkolaczk@datastax.com>
2019-07-18 09:47:06 +0200

    Use Platform JVM when compiling
    Fixes #968

---
+++ backend/src/main/scala/bloop/JdkInstance.scala
@@ -0,0 +1,20 @@
+package bloop
+
+import java.nio.file.{Path, Paths}
+
+import bloop.io.AbsolutePath
+
+case class JdkInstance private (jdkHome: AbsolutePath, options: Seq[String] = Seq.empty)
+
+object JdkInstance {
+
+  def apply(javaHome: AbsolutePath, options: Seq[String] = Seq.empty): JdkInstance = {
+    val jdkHome = if (javaHome.toString.endsWith("jre")) javaHome.getParent else javaHome
+    new JdkInstance(jdkHome, options)
+  }
+
+  val Default: JdkInstance = {
+    val javaHome = AbsolutePath(sys.props("java.home"))
+    JdkInstance(javaHome)
+  }
+}
